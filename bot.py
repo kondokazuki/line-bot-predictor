0676ef61a0ba29d9ae8e9474db6396a81b970ca1
kondokazuki <48248052+kondokazuki@users.noreply.github.com>
2025-04-27 22:45:57 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,19 +1,15 @@
 from flask import Flask, request, abort
-from linebot.v3.messaging import MessagingApiClient
-from linebot.v3.messaging.models import ReplyMessageRequest, TextMessage
-from linebot.v3.webhook import WebhookHandler
-from linebot.v3.webhook.models import MessageEvent
-from linebot.v3.exceptions import InvalidSignatureError
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import pandas as pd
 import os
-
-
 
 # LINEチャネル情報
 CHANNEL_ACCESS_TOKEN = '0lqYCmSUQjUGdpwk77aNZ8cEXe75Rlz509cftBA2F1EaJDSLXLLBBF9W4unatBKQJlPIDm02YOWaxpZaFU1qOolz99MTzRzrtT2p1PDEr+E/jYM5tMYpox5i/pbxTvwhcdsgDiQUq55+aJwpp0EkTwdB04t89/1O/w1cDnyilFU='
 CHANNEL_SECRET = '13f4af9e18a2f1bf4606703527353227'
 
-line_bot_api = MessagingApiClient(CHANNEL_ACCESS_TOKEN)
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 app = Flask(__name__)
@@ -103,10 +99,8 @@ def handle_message(event):
     result = predict_from_input(user_input)
 
     line_bot_api.reply_message(
-        ReplyMessageRequest(
-            reply_token=event.reply_token,
-            messages=[TextMessage(text=result)]
-        )
+        event.reply_token,
+        TextSendMessage(text=result)
     )
 
 if __name__ == "__main__":
